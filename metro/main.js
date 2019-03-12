5a19354cb7442b7090f4b2d7cf3570af2c21b9ff
Oleg Voloshyn <vololeg93@gmail.com>
2019-03-12 16:14:22 +0200

    added metro folder to get started a new project

---
+++ metro/main.js
@@ -0,0 +1,127 @@
+let metroLines = [{
+    color: 'Green',
+    stations: [
+    {
+        name: 'Syrets'
+    }, {
+        name: 'Dorohozhychi'
+    }, {
+        name: 'Lukianivska'
+    }, {
+        name: 'Lvivska Brama'
+    }, {
+        name: 'Zoloti Vorota'
+    }, {
+        name: 'Palats Sportu'
+    }, {
+        name: 'Klovska'
+    }, {
+        name: 'Pecherska'
+    }, {
+        name: 'Druzhby Narodiv'
+    }, {
+        name: 'Vydubychi'
+    }, {
+        name: 'Telychka'
+    }, {
+        name: 'Slavutych'
+    }, {
+        name: 'Osokorky'
+    }, {
+        name: 'Pozniaky'
+    }, {
+        name: 'Kharkivska'
+    }, {
+        name: 'Vyrlytsia'
+    }, {
+        name: 'Boryspilska'
+    }, {
+        name: 'Chervony Khutir'
+    }]
+}, {
+    color: 'Red',
+    stations: [
+    {
+        name: 'Akademmistechko'
+    }, {
+        name: 'Zhytomyrska'
+    }, {
+        name: 'Sviatoshyn'
+    }, {
+        name: 'Nyvky'
+    }, {
+        name: 'Beresteiska'
+    }, {
+        name: 'Shuliavska'
+    }, {
+        name: 'Politekhnichnyi Instytut'
+    }, {
+        name: 'Vokzalna'
+    }, {
+        name: 'Universytet'
+    }, {
+        name: 'Teatralna'
+    }, {
+        name: 'Khreschatyk'
+    }, {
+        name: 'Arsenalna'
+    }, {
+        name: 'Dnipro'
+    }, {
+        name: 'Hidropark'
+    }, {
+        name: 'Livoberezhna'
+    }, {
+        name: 'Darnytsia'
+    }, {
+        name: 'Chernihivska'
+    }, {
+        name: 'Lisova'
+    }]
+}, {
+    color: 'Blue',
+    stations: [
+        {
+        name: 'Heroiv Dnipra'
+    }, {
+        name: 'Minska'
+    }, {
+        name: 'Obolon'
+    }, {
+        name: 'Petrivka'
+    }, {
+        name: 'Tarasa Shevchenka'
+    }, {
+        name: 'Kontraktova Ploshcha'
+    }, {
+        name: 'Poshtova Ploshcha'
+    }, {
+        name: 'Maidan Nezalezhnosti'
+    }, {
+        name: 'Ploshcha Lva Tolstoho'
+    }, {
+        name: 'Olimpiiska'
+    }, {
+        name: 'Palats "Ukrayina" '
+    }, {
+        name: 'Lybidska'
+    }, {
+        name: 'Demiivska'
+    }, {
+        name: 'Holosiivska'
+    }, {
+        name: 'Vasylkivska'
+    }, {
+        name: 'Vystavkovyi Tsentr'
+    }, {
+        name: 'Ipodrom'
+    }, {
+        name: 'Teremky'
+    }]
+}];
+
+console.log(metroLines);
+
+let schedule = [
+
+];
